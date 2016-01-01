4d9bf3c462b6f8df831c42b59701841e4e120311
Nathan Fennel <nathanfennel@gmail.com>
2016-01-01 08:56:59 -0800

    NKFToolbar: Added to project

---
+++ Toolbar Test/NKFToolbar.h
@@ -0,0 +1,13 @@
+//
+//  NKFToolbar.h
+//  Toolbar Test
+//
+//  Created by HAI on 1/1/16.
+//  Copyright © 2016 Nathan Fennel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NKFToolbar : UIToolbar
+
+@end
---
+++ Toolbar Test/NKFToolbar.m
@@ -0,0 +1,13 @@
+//
+//  NKFToolbar.m
+//  Toolbar Test
+//
+//  Created by HAI on 1/1/16.
+//  Copyright © 2016 Nathan Fennel. All rights reserved.
+//
+
+#import "NKFToolbar.h"
+
+@implementation NKFToolbar
+
+@end
